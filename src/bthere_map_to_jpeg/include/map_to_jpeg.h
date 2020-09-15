d71bebe48b4af90d4d4a7c8f8be860b421d4fd8c
Matt Wenger <matt@bthere.ai>
2020-09-15 15:19:05 -0700

    Display occupancy grid as a ROS image topic

--- src/bthere_map_to_jpeg/include/map_to_jpeg.h
+++ src/bthere_map_to_jpeg/include/map_to_jpeg.h
@@ -2,6 +2,7 @@
 
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <tf2_ros/transform_listener.h>
 #include <opencv2/core.hpp>
 
 namespace bthere_map_to_jpeg
@@ -35,10 +36,24 @@ namespace bthere_map_to_jpeg
         // Sequence counter
         int seq_cnt_;
 
+        // Buffer & TransformListener for receiving the robot transform
+        tf2_ros::Buffer tfBuffer_;
+        tf2_ros::TransformListener *tfListener_;
+
+    public:
+    // Map origin
+        float map_origin_x;
+        float map_origin_y;
+
+        // Occupancy grid resolution
+        float og_res_;
+
+        // Region of interest
+        cv::Rect roi_;
+
         // Image to publish
         cv::Mat image_;
 
-    public:
         /**
         @brief Default constructor
         @param argc [int] Number of input arguments
@@ -53,6 +68,17 @@ namespace bthere_map_to_jpeg
         **/
         ~MapToJpeg(void);
 
+        
+
+        cv::Mat convertOccupancyGridToMat(const nav_msgs::OccupancyGrid &occupancy_grid);
+
+        /**
+        @brief Extract the region of interest from the input
+        @param input [cv::Mat] An openCV matrix representing an occupancy grid as an image
+        @return [cv::Rect] An openCV Rect that specifies the region of interest
+        **/ 
+        cv::Rect getROI(cv::Mat input);
+
     private:
         /**
         @brief Callback for the subscriber
@@ -61,6 +87,8 @@ namespace bthere_map_to_jpeg
         **/
         void callback(const nav_msgs::OccupancyGrid& msg);
 
-        cv::Mat getROI(cv::Mat input);
+        static void processOccupancyGrid(MapToJpeg* instance, const nav_msgs::OccupancyGrid &grid);
+
+        cv::Mat overlayRobotLocation(cv::Mat input, cv::Rect roi = cv::Rect());
     };
 } // namespace bthere_map_to_jpeg
--- src/bthere_map_to_jpeg/src/bthere_map_to_jpeg.cpp
+++ src/bthere_map_to_jpeg/src/bthere_map_to_jpeg.cpp
@@ -6,6 +6,8 @@
 #include <sensor_msgs/image_encodings.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <sensor_msgs/CompressedImage.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <vector>
 #include <grid_map_ros/grid_map_ros.hpp>
 #include <grid_map_msgs/GridMap.h>
@@ -14,12 +16,18 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <thread>
 
 static const std::string OPENCV_WINDOW = "Image window";
+static const float RADIUS_OF_ROBOT = 0.2;
+
 
 namespace bthere_map_to_jpeg
 {
-
+    /**
+     * Helper function for obtaining the string format of a cv::Mat element type
+     * @param type type of a matrix element
+     */
     std::string type2str(int type)
     {
         std::string r;
@@ -61,6 +69,10 @@ namespace bthere_map_to_jpeg
         return r;
     }
 
+
+    /**
+     * ROS node converting a nav_msgs/OccupancyGrid.msg topic into a sensor_msgs/Image.msg topic
+     */
     MapToJpeg::MapToJpeg(int argc, char **argv)
     {
 
@@ -73,6 +85,8 @@ namespace bthere_map_to_jpeg
             ROS_ERROR("Usage: bthere_map_to_jpeg bthere_map_to_jpeg_node <occupancy_grid_topic> <image_topic_namespace>");
             exit(0);
         }
+
+        tfListener_ = new tf2_ros::TransformListener(tfBuffer_);
 
         std::string image_topic, compressed_image_topic;
 
@@ -100,11 +114,19 @@ namespace bthere_map_to_jpeg
         {
             if (!image_.empty())
             {
+                cv::Mat flipped_image, image_with_overlay, flipped_image_with_overlay;
+                
+                // Overlay the robot location on the occupancy grid image
+                image_with_overlay = overlayRobotLocation(image_, roi_);
+
+                // Before publishing the image flip it about the horizontal axis
+                cv::flip(image_with_overlay, flipped_image_with_overlay, 0);
+
                 // ROS_INFO("Creating image message");
                 std_msgs::Header header;
                 header.seq = seq_cnt_++;
                 header.stamp = ros::Time::now();
-                img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, image_);
+                img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::BGR8, flipped_image_with_overlay);
                 img_bridge.toImageMsg(img_msg);
 
                 // ROS_INFO("Creating compressed image message");
@@ -112,8 +134,14 @@ namespace bthere_map_to_jpeg
 
                 // ROS_INFO("Publishing image message");
                 image_pub_.publish(img_msg);
+
                 // ROS_INFO("Publishing compressed image message");
                 compressed_image_pub_.publish(c_img_msg);
+
+                // If running on robot with a GUI, then display the image
+                cv::namedWindow(OPENCV_WINDOW, cv::WINDOW_AUTOSIZE);
+                cv::imshow(OPENCV_WINDOW, flipped_image_with_overlay);
+                cv::waitKey(100);
             }
 
             ros::spinOnce();
@@ -122,87 +150,87 @@ namespace bthere_map_to_jpeg
         }
     }
 
+
+    /**
+     * Class destructor
+     */
     MapToJpeg::~MapToJpeg(void)
     {
         cv::destroyWindow(OPENCV_WINDOW);
     }
 
-    void MapToJpeg::callback(const nav_msgs::OccupancyGrid &msg)
+
+    /**
+     * Coverts an occupany grid to an opencv image
+     * @param grid A reference to a nav_msgs::OccupancyGrid
+     * 
+     * @return  An opencv greyscale image where the occupancy grid values (-1, 0, 100) have been converted to a
+     *          grey, white, and black respectively.
+     */
+    cv::Mat MapToJpeg::convertOccupancyGridToMat(const nav_msgs::OccupancyGrid &grid)
     {
-
-        ROS_INFO("\nReceived an OccupancyGrid message");
-
-        const nav_msgs::OccupancyGrid &occupancy_grid = (nav_msgs::OccupancyGrid &)msg;
-
-        ROS_INFO("Width: %d, Heigth: %d", occupancy_grid.info.width, occupancy_grid.info.height);
-        ROS_INFO("Size of data : %ld", occupancy_grid.data.size());
-        ROS_INFO("Origin (x: %f, y: %f, z: %f)",
-            (occupancy_grid.info.width / 2) + occupancy_grid.info.origin.position.x,
-            (occupancy_grid.info.height / 2) + occupancy_grid.info.origin.position.y,
-            occupancy_grid.info.origin.position.z
-            );
-
-        ROS_INFO("Converting OccupancyGrid to Mat");
         int row, col, value;
         int array_index = 0;
-        cv::Mat single_channel_image(occupancy_grid.info.width, occupancy_grid.info.height, CV_8UC1);
-        for(std::vector<int8_t>::const_iterator it = occupancy_grid.data.begin(); it != occupancy_grid.data.end(); ++it, array_index++) {
-            row = (int)array_index/single_channel_image.cols;
-            col = array_index%single_channel_image.cols;
+        cv::Mat image(grid.info.width, grid.info.height, CV_8UC1);
+
+        for(std::vector<int8_t>::const_iterator it = grid.data.begin(); it != grid.data.end(); ++it, array_index++) {
+            row = (int)array_index/image.cols;
+            col = array_index%image.cols;
             if ((int)*it == -1) {
-                value = 125;
+                value = 125;    // grey
             } else if ((int)*it == 100) {
-                value = 0;
+                value = 0;      // black
             } else if ((int)*it == 0) {
-                value = 255;
+                value = 255;    // white
             } else {
                 ROS_WARN("Unsupported value in Occupancy Grid");
                 value == 125;
             }
-            single_channel_image.at<uchar>(row, col) = (uchar)value;
+            image.at<uchar>(row, col) = (uchar)value;
         }
+        return image;
+    }
+
+
+    /**
+     * Callback function that is called each time a new map is published
+     * @param msg   A message containing the occupancy grid information
+     */
+    void MapToJpeg::callback(const nav_msgs::OccupancyGrid &msg)
+    {
+        printf("\n");
+        ROS_INFO("Received an OccupancyGrid message");
+
+        // Since processing the occupancy grid information can take some time, let's do this in another thread
+        new std::thread(processOccupancyGrid, this, msg);
+    }
+
+
+    /**
+     * Processes the incoming occupancy grid data by converting it to an opencv image and cropping the image to
+     * an region of interest.
+     * @param instance  The class instance associated with the callback
+     * @param grid      The occupancy grid data
+     */
+    void MapToJpeg::processOccupancyGrid(MapToJpeg *instance, const nav_msgs::OccupancyGrid &grid)
+    {
+        instance->map_origin_x = grid.info.width / 2;
+        instance->map_origin_y = grid.info.height / 2;
+        instance->og_res_ = grid.info.resolution;
+
+        ROS_INFO("Width: %d, Heigth: %d", grid.info.width, grid.info.height);
+        ROS_INFO("Size of data : %ld", grid.data.size());
+        ROS_INFO("Origin (x: %f, y: %f, z: %f)",
+            instance->map_origin_x,
+            instance->map_origin_y,
+            grid.info.origin.position.z
+            );
+        ROS_INFO("Resolution : %f", instance->og_res_);
+
+        // Convert the occupancy grid to an OpenCV matrix
+        ROS_INFO("Converting OccupancyGrid to Mat");
+        cv::Mat single_channel_image = instance->convertOccupancyGridToMat(grid);
         ROS_INFO("Done!");
-
-        // cv::Mat roi = getROI(mapped_og);
-
-        // ROS_INFO("Writing image to file");
-        // cv::imwrite("image.jpeg", roi);
-
-        // ROS_INFO("Showing the image");
-        // cv::namedWindow(OPENCV_WINDOW, cv::WINDOW_AUTOSIZE);
-        // cv::imshow(OPENCV_WINDOW, roi);
-        // cv::waitKey(1000);
-
-        // #ifdef false
-        // ROS_INFO("Converting OccupancyGrid to GridMap");
-        // grid_map::GridMap map;
-        // // if (grid_map::GridMapRosConverter::fromOccupancyGrid((nav_msgs::OccupancyGrid &)msg, "elevation", map))
-        // if (grid_map::GridMapRosConverter::fromOccupancyGrid(occupancy_grid, "elevation", map))
-        // {
-        //     ROS_INFO("Success!");
-        //     std::cout << "GridMap size : " << map.getSize() << std::endl;
-        // }
-        // else
-        // {
-        //     ROS_WARN("Failure");
-        // }
-
-        // ROS_INFO("Converting GridMap to Image");
-        // cv::Mat single_channel_image;
-        // cv::Mat image;
-        // if (grid_map::GridMapCvConverter::toImage<unsigned char, 1>(map, "elevation", CV_8UC1, 100, 0, single_channel_image))
-        // {
-        //     ROS_INFO("Success!");
-        //     cv::Mat roi = getROI(single_channel_image);
-        //     cv::cvtColor(roi, image, CV_GRAY2BGR);
-        //     ROS_INFO("Number of channels: %d", image.channels());
-        //     ROS_INFO("Columns: %d, Rows: %d", image.cols, image.rows);
-        //     // std::cout << "image: " << single_channel_image.row(0) << std::endl;
-        // }
-        // else
-        // {
-        //     ROS_WARN("Failure");
-        // }
 
         if (single_channel_image.empty())
         {
@@ -211,33 +239,39 @@ namespace bthere_map_to_jpeg
         }
         else
         {
-            cv::Mat image, flipped_image;
+            cv::Mat multi_channel_image;
             
             // Get the region of interest
-            cv::Mat roi = getROI(single_channel_image);
+            instance->roi_ = instance->getROI(single_channel_image);
+
+            // Update the image parameters
+            instance->n_.setParam(instance->image_topic_namespace_ + std::string("/image_width"), instance->roi_.width);
+            instance->n_.setParam(instance->image_topic_namespace_ + std::string("/image_height"), instance->roi_.height);
 
             // Convert single_channel_image to 3 channels
-            cv::cvtColor(roi, image, CV_GRAY2BGR);
+            cv::cvtColor(single_channel_image, multi_channel_image, CV_GRAY2BGR);
 
-            // Flip image about the horizontal axis
-            cv::flip(image, flipped_image, 0);
+            // Crop the image to the ROI
+            cv::Mat image_to_display = multi_channel_image(instance->roi_);
 
-            // ROS_INFO("Writing image to file");
-            cv::imwrite("image.jpeg", flipped_image);
-            flipped_image.copyTo(image_);
-
-            ROS_INFO("Showing the image");
-            cv::namedWindow(OPENCV_WINDOW, cv::WINDOW_AUTOSIZE);
-            cv::imshow(OPENCV_WINDOW, flipped_image);
-            cv::waitKey(1000);
+            // Make the image available to the main loop
+            image_to_display.copyTo(instance->image_);
         }
-        // #endif
     }
 
-    cv::Mat MapToJpeg::getROI(cv::Mat input)
+
+    /**
+     * Get a bounding rectangle that represents the region of interest within a single channel image.
+     * Since the occupancy grid is mainly filled with -1 values representing 'unknown' occupancy, getROI
+     * will look for cells that contain other values to determine a bounding rectangle around the region.
+     * @param input The image on which to find the region of interest
+     * 
+     * @return  A bounding rectangle representing the region of interest
+     */
+    cv::Rect MapToJpeg::getROI(cv::Mat input)
     {
         int threshold = 250;
-        int offset = 100;
+        int offset = 10;
         int min_count = 2;
         int scan_offset = 10;
 
@@ -246,23 +280,22 @@ namespace bthere_map_to_jpeg
         int count;
 
         std::string ty = type2str(input.type());
-        printf("Matrix: %s %dx%d \n", ty.c_str(), input.cols, input.rows);
+        ROS_INFO("Matrix: %s %dx%d", ty.c_str(), input.cols, input.rows);
 
         if (input.empty()) {
             ROS_WARN("Image is empty!");
         }
 
-        // Get the first row that contains data
+        // Get the first row (top) that contains data
         found = false;
         count = 0;
-        for (int row = scan_offset; row < input.rows; row++)
+        int row, col;
+        for (row = scan_offset; row < input.rows; row++)
         {
-            for (int col = scan_offset; col < input.cols; col++)
+            for (col = scan_offset; col < input.cols; col++)
             {
-                // std::cout << "row: " << row << ", col: " << col << std::endl;
                 if (input.at<uchar>(row, col) > threshold)
                 {
-                    std::cout << "top: " << row << ", " << col << " [" << (int)input.at<uchar>(row, col) << "]" << std::endl;
                     if (count++ >= min_count)
                     {
                         top = row;
@@ -271,21 +304,22 @@ namespace bthere_map_to_jpeg
                     }
                 }
             }
-            if (found)
+            if (found) {
+                // std::cout << "top: " << row << " [" << (int)input.at<uchar>(row, col) << "]" << std::endl;
                 break;
+            }
         }
         // std::cout << "Finished finding first row!" << std::endl;
 
-        // Get the first column that contains data
+        // Get the first column (left) that contains data
         found = false;
         count = 0;
-        for (int col = scan_offset; col < input.cols; col++)
+        for (col = scan_offset; col < input.cols; col++)
         {
-            for (int row = scan_offset; row < input.rows; row++)
+            for (row = scan_offset; row < input.rows; row++)
             {
                 if (input.at<uchar>(row, col) > threshold)
                 {
-                    // std::cout << "left: " << row << ", " << col << " [" << (int)input.at<uchar>(row, col) << "]" << std::endl;
                     if (count++ >= min_count)
                     {
                         left = col;
@@ -294,21 +328,22 @@ namespace bthere_map_to_jpeg
                     }
                 }
             }
-            if (found)
+            if (found) {
+                // std::cout << "left: " << col << " [" << (int)input.at<uchar>(row, col) << "]" << std::endl;
                 break;
+            }
         }
         // std::cout << "Finished finding first colum!" << std::endl;
 
-        // Get the last row that contains data
+        // Get the last row (bottom) that contains data
         found = false;
         count = 0;
-        for (int row = input.rows - scan_offset; row >= 0; row--)
+        for (row = input.rows - scan_offset; row >= 0; row--)
         {
-            for (int col = input.cols - scan_offset; col >= 0; col--)
+            for (col = input.cols - scan_offset; col >= 0; col--)
             {
                 if (input.at<uchar>(row, col) > threshold)
                 {
-                    // std::cout << "bottom: " << row << ", " << col << " [" << (int)input.at<uchar>(row, col) << "]" << std::endl;
                     if (count++ >= min_count)
                     {
                         bottom = row;
@@ -317,21 +352,22 @@ namespace bthere_map_to_jpeg
                     }
                 }
             }
-            if (found)
+            if (found) {
+                // std::cout << "bottom: " << row << " [" << (int)input.at<uchar>(row, col) << "]" << std::endl;
                 break;
+            }
         }
         // std::cout << "Finished finding last row!" << std::endl;
 
-        // Get the last column that contains data
+        // Get the last column (right) that contains data
         found = false;
         count = 0;
-        for (int col = input.cols - scan_offset; col >= 0; col--)
+        for (col = input.cols - scan_offset; col >= 0; col--)
         {
-            for (int row = input.rows - scan_offset; row >= 0; row--)
+            for (row = input.rows - scan_offset; row >= 0; row--)
             {
                 if (input.at<uchar>(row, col) > threshold)
                 {
-                    // std::cout << "right: " << row << ", " << col << " [" << (int)input.at<uchar>(row, col) << "]" << std::endl;
                     if (count++ >= min_count)
                     {
                         right = col;
@@ -340,8 +376,10 @@ namespace bthere_map_to_jpeg
                     }
                 }
             }
-            if (found)
+            if (found) {
+                // std::cout << "right: " << col << " [" << (int)input.at<uchar>(row, col) << "]" << std::endl;
                 break;
+            }
         }
         // std::cout << "Finished finding last column!" << std::endl;
 
@@ -349,23 +387,48 @@ namespace bthere_map_to_jpeg
         left = (left - offset) > 0 ? left - offset : left;
         bottom = (bottom + offset) > input.rows ? bottom : bottom + offset;
         right = (right + offset) > input.cols ? right : right + offset;
+        ROS_INFO("ROI: %d, %d, %d, %d", left, top, right, bottom);
 
-        input.row(top).setTo(cv::Scalar(255));
-        input.col(left).setTo(cv::Scalar(255));
-        input.row(bottom).setTo(cv::Scalar(255));
-        input.col(right).setTo(cv::Scalar(255));
-        ROS_INFO("ROI: %d, %d, %d, %d", top, left, bottom, right);
+        cv::Rect R(left, top, right - left, bottom - top);
+        return R;
+    }
 
-        int width = right - left;
-        // ROS_INFO("width : %d", width);
-        int height = bottom - top;
-        // ROS_INFO("height : %d", height);
-        n_.setParam(image_topic_namespace_ + std::string("/image_width"), width);
-        n_.setParam(image_topic_namespace_ + std::string("/image_height"), height);
 
-        // ROS_INFO("Cropping to RECT(%d, %d, %d, %d)", left, top, width, height);
-        cv::Rect R(left, top, width, height);
-        return input(R);
+    /**
+     * Overlays the robot's location over the input image of the occupancy grid cropped to the region of interest.
+     * @param input cv::Mat representation of the occupancy grid cropped to the region of interest
+     * @param roi   cv::Rect the region of interest that was used to crop the image
+     */
+    cv::Mat MapToJpeg::overlayRobotLocation(cv::Mat input, cv::Rect roi)
+    {
+        cv::Mat output(input.size().width, input.size().height, input.type());
+        input.copyTo(output);
+        
+        // Get the robot pose
+        geometry_msgs::TransformStamped transformStamped;
+        try {
+            // Get the current tranform of the robot
+            transformStamped = tfBuffer_.lookupTransform("world", "robot0", ros::Time(0));
+            // ROS_INFO("transform (x: %f, y: %f, z: %f)",
+            //     transformStamped.transform.translation.x / og_res_,
+            //     transformStamped.transform.translation.y / og_res_,
+            //     transformStamped.transform.translation.z / og_res_
+            //     );
+
+            // Find the location of the robot
+            cv::Point robot_center;
+            robot_center.x = map_origin_x - roi.x + (transformStamped.transform.translation.x / og_res_);;
+            robot_center.y = map_origin_y - roi.y + (transformStamped.transform.translation.y / og_res_);;
+            // ROS_INFO("Robot center x: %d, y: %d", robot_center.x, robot_center.y);
+
+            // Draw a circle to represent the robot
+            circle(output, robot_center, RADIUS_OF_ROBOT/og_res_, cv::Scalar(0, 0, 255), -1);
+        }
+        catch (tf2::TransformException &ex) {
+            ROS_WARN("%s", ex.what());
+        }
+
+        return output;
     }
 
 } // namespace bthere_map_to_jpeg
